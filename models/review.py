faca46f3d51e94696e0ce0200e6f65cd427b54ec
mubaraq <paynedaze@gmail.com>
2022-08-01 18:27:02 +0100

    models

---
+++ models/review.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Defines review class
+"""
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """Reviews made by users about a place"""
+    place_id = ""
+    user_id = ""
+    text = ""
+    
